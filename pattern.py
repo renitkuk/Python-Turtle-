b78c70f44b0f16b9ff7c88ca810cea8d89e5bb2a
Renit <renitkukreti@gmail.com>
2026-09-18 10:49:41 +0530

    Turtle graphics pattern drawing script

---
+++ pattern.py
@@ -0,0 +1,11 @@
+import turtle, colorsys
+screen = turtle.Screen()
+screen.bgcolor("black")
+t=turtle.Turtle()
+t.speed(0)
+t.hideturtle()
+for i in range(150):
+    t.pencolor(colorsys.hsv_to_rgb(0.12-i/150*0.12,1,1))
+    t.forward(i*2.8)
+    t.right(165)
+turtle.done()
